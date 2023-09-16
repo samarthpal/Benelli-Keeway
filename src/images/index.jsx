d85d43153719c7fa1e082497c7c757d91e6c965a
Samarth kumar pal <palsamarth02@gmail.com>
2023-09-16 21:21:09 +0530

    Deploy done

--- src/images/index.jsx
+++ src/images/index.jsx
@@ -1,12 +1,13 @@
-import back from "./illus/back.png";
-import bike1 from "./logos/bike1.png";
-import bike2 from "./logos/bike2.png";
-import facebook from "./logos/facebook.png";
-import instagram from "./logos/instagram.png";
-import logo1 from "./logos/logo1.png";
-import logo2 from "./logos/logo2.png";
-import review from "./logos/review.png";
-import youtube from "./logos/youtube.png";
+import back from './illus/back.png'
+import bike1 from './logos/bike1.png'
+import bike2 from './logos/bike2.png'
+import facebook from './logos/facebook.png'
+import instagram from './logos/instagram.png'
+import logo from './logos/logo.png'
+import logo1 from './logos/logo1.png'
+import logo2 from './logos/logo2.png'
+import review from './logos/review.png'
+import youtube from './logos/youtube.png'
 
 const main = {
   back,
@@ -14,10 +15,11 @@ const main = {
   bike2,
   facebook,
   instagram,
+  logo,
   logo1,
   logo2,
   review,
-  youtube,
-};
+  youtube
+}
 
-export default main;
+export default main
