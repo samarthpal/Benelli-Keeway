79f9f0a62078cdb3c090b76df87c7e006e8db46d
Samarth kumar pal <palsamarth02@gmail.com>
2023-09-16 21:05:57 +0530

    Improvement

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,5 @@
-import {
-  Route,
-  BrowserRouter as Router,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
-
-import routes from "./routes";
-import { useEffect } from "react";
+import SocialPage from './socialPage'
 
 export default function Main() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Redirect />} />
-
-        {routes.map(({ path, Component }, i) => (
-          <Route key={i} path={path} element={<Component />} />
-        ))}
-
-        <Route path="*" element={<Redirect />} />
-      </Routes>
-    </Router>
-  );
+  return <SocialPage />
 }
-
-const Redirect = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/home", { replace: true });
-  }, [navigate]);
-  return null;
-};
