95a47fc999d19459cf3bfc739a4406f892665a39
Samarth kumar pal <palsamarth02@gmail.com>
2023-09-16 19:38:51 +0530

    Done

---
+++ src/images/index.jsx
@@ -0,0 +1,23 @@
+import back from "./illus/back.png";
+import bike1 from "./logos/bike1.png";
+import bike2 from "./logos/bike2.png";
+import facebook from "./logos/facebook.png";
+import instagram from "./logos/instagram.png";
+import logo1 from "./logos/logo1.png";
+import logo2 from "./logos/logo2.png";
+import review from "./logos/review.png";
+import youtube from "./logos/youtube.png";
+
+const main = {
+  back,
+  bike1,
+  bike2,
+  facebook,
+  instagram,
+  logo1,
+  logo2,
+  review,
+  youtube,
+};
+
+export default main;
--- src/pages/socialPage/index.jsx
+++ src/pages/socialPage/index.jsx
@@ -1,5 +1,65 @@
-import s from "./styles.module.scss";
+import { Link } from 'react-router-dom'
+import s from './styles.module.scss'
+import images from 'images'
+
+const linkData = [
+  {
+    field: 'SHOWROOM REVIEWS',
+    link: '/',
+    image: images.review
+  },
+  {
+    field: 'WORKSHOP REVIEWS',
+    link: '/',
+    image: images.review
+  },
+  {
+    field: 'INSTAGRAM',
+    link: '/',
+    image: images.instagram
+  },
+  {
+    field: 'FACEBOOK',
+    link: '/',
+    image: images.facebook
+  },
+  {
+    field: 'YOUTUBE',
+    link: '/',
+    image: images.youtube
+  }
+]
 
 export default function Main() {
-  return <div className={s.main}>page</div>;
+  return (
+    <div className={s.main}>
+      <div className={s.inner}>
+        <div className={s.logo}>
+          <img src={images.logo1} alt='' />
+        </div>
+        <div className={s.poweredByLogo}>
+          <img src={images.logo2} alt='' />
+        </div>
+        <div className={s.title}>Click Here For</div>
+        <div className={s.links}>
+          {linkData.map(({ field, link, image }, i) => (
+            <Link to={link} key={i} className={s.link} target='_blank'>
+              <div>
+                <img src={image} alt='' />
+              </div>
+              <div>{field}</div>
+            </Link>
+          ))}
+        </div>
+        <div className={s.bottom}>
+          <div className={s.left}>
+            <img src={images.bike1} alt='' />
+          </div>
+          <div className={s.right}>
+            <img src={images.bike2} alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
